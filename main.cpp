4d212d1e2858b465d21776b81921aa32af8831cd
celestia520 <wiwianita876@gmail.com>
2025-10-31 18:31:38 +0100

    feat :add input functionality with proper code

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+int num1 ,int num2;
+cout <<"enter first number : ";
+cin >>num1;
+cout <<"enter the second number :";
+cin >>num2;
+return 0;
